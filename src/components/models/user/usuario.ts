0699660b4345f23dda4bc77f471115ba1737ec4e
Edwin Geo <chumbi21b@gmail.com>
2026-01-02 03:31:56 +0100

    public

--- src/components/models/user/usuario.ts
+++ src/components/models/user/usuario.ts
@@ -2,7 +2,7 @@ export class Usuario {
   clientId: string = 'asdfasfasdfasdf';
   clientSecret: string = 'asdfasdfa';
   username: string = 'adsfasdfasfasdfasdf';
-  password: string = 'asdf%';
+  password: string = 'asdfd%';
   grant_type: string = 'password';
 
 }
